c9d35a802ada7c471cae23506b3b516bbfddd5e0
JwDevilx <JWdevilx@proton.me>
2024-05-27 11:04:21 +0530

    Update Activity2.java

--- src/Activity2.java
+++ src/Activity2.java
@@ -3,6 +3,7 @@ public class Activity2 {
         int Marks01 =50;
         int Marks02 =45;
         int Marks03 =45;
+        int Marks03 =60;
 
         int TotalMarks = Marks01+Marks02+Marks03;
         System.out.println("TotalMarks "+TotalMarks);
